9329591a0a3f8fb95792475d383281c277753258
RykyCE <rcastilla744@gmail.com>
2023-11-11 10:31:48 -0500

    Supporting borders and padding

--- src/components/ViewportProvider.tsx
+++ src/components/ViewportProvider.tsx
@@ -2,15 +2,6 @@ import { LayoutChangeEvent, StyleSheet, View } from 'react-native'
 import React, { ReactElement, useState } from 'react'
 import ViewportContext, { initValue, Layout } from '../viewport_context'
 
-//  1.1.0
-// utilizar context API para valores de viewport
-// posibilidad de tener varios contextos (hasta aqui hay un mismo commit)
-// incluir stylesheet
-// permitir useViewport en los bordes en los bordes
-// incluir todas las expresiones regulares en un mismo fichero
-// incluir evento al cargar viwport
-// actualizar documentcion
-
 function onLayout( event:LayoutChangeEvent, setDimensions:React.Dispatch<React.SetStateAction<{width:number,height:number}>> ) {
   const { width, height } = event.nativeEvent.layout
   const layout: Layout = { width, height }
