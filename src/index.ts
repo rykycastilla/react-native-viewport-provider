582b700c7a2b44799b5738d85387d875c8ab18ba
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2023-12-03 11:20:15 -0500

    Supporting "useVP" alias of "useViewport"

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as useDimensions } from './hooks/dimensions'
-export { default as useViewport } from './hooks/viewport'
+export { default as useViewport, default as useVP } from './hooks/viewport'
 export { default, default as ViewportProvider } from './components/ViewportProvider'
 export { Layout } from './viewport_context'
